1312026865b8b86d266c1a5b5dfbfbc3c7c7f1ac
Rakka Alhazimi <rakkakeren@gmail.com>
2023-12-22 01:42:17 +0700

    add: emu otori popup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,134 @@
-import Image from 'next/image'
+"use client"
+import { useRef, useState } from "react"
+
+
+
+const IMG_WIDTH = 250
+const IMG_HEIGHT = 216
+// Default element placement will be altered when we rotate the image.
+// For instance, when we move the element to the top-left corner,
+// you will get your element out of page when you perform rotation on it.
+// That is why we need to readjust the placement.
+const OFFSET = Math.floor(Math.abs(IMG_HEIGHT - IMG_WIDTH) / 2)
+const SHIFT_FACTORS = [0, 25, 50, 75, 100]
+
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left
+}
+
+function randint(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
 
 export default function Home() {
+  
+  const audioRef = useRef<HTMLSourceElement>(null)
+  const [emuArray, setEmuArray] = useState<JSX.Element[]>([])
+  
+  function EmuStamp() {
+    const emuRef = useRef<HTMLImageElement>(null)
+    setTimeout(() => {
+      // emuRef.current?.remove()
+    }, 1500)
+    
+    let direction = Math.floor(Math.random() * Object.keys(Direction).length / 2)
+    let verticalShift = randint(OFFSET, window.innerHeight - IMG_WIDTH)
+    let horizontalShift = randint(0, window.innerWidth - IMG_WIDTH)
+    let style = {}
+    
+    switch (direction) {
+      default:
+      case Direction.Up:
+        style = {
+          position: 'absolute',
+          top: '0px',
+          left: `${horizontalShift}px`,
+          transform: 'rotate(180deg)'
+        }
+        // console.log("up")
+        break;
+        
+      case Direction.Right:
+        style = {
+          position: 'absolute',
+          top: `${verticalShift}px`,
+          right: `-${OFFSET}px`,
+          transform: 'rotate(-90deg)'
+        }
+        // console.log("right")
+        break;
+        
+      case Direction.Down:
+        style = {
+          position: 'absolute',
+          bottom: '0px',
+          left: `${horizontalShift}px`,
+        }
+        // console.log("down")
+        break;
+        
+      case Direction.Left:
+        style = {
+          position: 'absolute',
+          top: `${verticalShift}px`,
+          left: `-${OFFSET}px`,
+          transform: 'rotate(90deg)'
+        }
+        // console.log("left ", verticalShift)
+        break;
+    }
+    
+    return (
+      <img 
+        ref={emuRef}
+        style={style}
+        width={IMG_WIDTH}
+        height={IMG_HEIGHT}
+        src="/Emu_Stamp_Wandahoi.webp"
+      />
+    )
+  }
+  
+  function playAudio() {
+    const audioAPI = new Audio()
+    
+    if (audioRef.current != null) {
+      audioAPI.appendChild(audioRef.current)
+      audioAPI.play()
+    }
+    
+    const myuuid = crypto.randomUUID()
+    setEmuArray([...emuArray, <EmuStamp key={myuuid}></EmuStamp>])
+    
+  }
+  
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <h2 className="absolute top-1 left-1 z-50">Emu count: {emuArray.length}</h2>
+      <h1
+        className={
+          "absolute bottom-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 " +
+          "text-7xl font-bold "
+        }
+      >
+        Emu Otori
+      </h1>
+      <source hidden ref={audioRef} src="/wonderhoi.mp3" type="audio/mpeg"></source>
+      <button 
+        className={
+          "h-auto py-3 px-6 " +
+          "absolute bottom-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 " +
+          "bg-pink-500 hover:bg-pink-600 text-gray-50 "
+        }
+        onClick={playAudio}>
+          Wandahoi !
+      </button>
+      {emuArray}
     </main>
   )
 }
